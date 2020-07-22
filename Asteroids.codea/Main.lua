d4e8e6fbd08337e1b22aa640d91fd65dd5279306
Philipp Hackbarth <hacku@users.noreply.github.com>
2020-07-22 22:07:00 +0200

    multiple moving asteroids

--- Asteroids.codea/Main.lua
+++ Asteroids.codea/Main.lua
@@ -3,8 +3,19 @@
 
 function setup()
     print("Hello Asteroids!")
-    pos = vec2(400,500)
-    angle = math.random() * 2 * math.pi
+    
+    asteroids = {}
+    
+    for i = 0,1 do
+        asteroid = {}
+        asteroid.pos = vec2(math.random(WIDTH),math.random(HEIGHT))
+        asteroid.angle = math.random() * 2 * math.pi
+    
+        table.insert(asteroids,asteroid)
+        
+    end
+    
+    
     vel = 1.5
 end
 
@@ -16,21 +27,26 @@ function draw()
     stroke(255)
     fill(2)
     rectMode(CENTER)
-    rect(pos.x,pos.y,120) 
     
-    -- velocity of asteroid
-    local step = vec2(vel,0):rotate(angle)
+    for i,asteroid in ipairs(asteroids) do
+        rect(asteroid.pos.x,asteroid.pos.y,120) 
     
-    -- move asteroid
-    pos = pos + step            
+        -- velocity of asteroid
+        local step = vec2(vel,0):rotate(asteroid.angle)
+        
+        -- move asteroid
+        asteroid.pos = asteroid.pos + step            
+        
+        
+        -- check screen boundaries
+        if asteroid.pos.x > WIDTH then asteroid.pos.x = asteroid.pos.x - WIDTH end
+        if asteroid.pos.x < 0 then asteroid.pos.x = asteroid.pos.x + WIDTH end
+        
+        if asteroid.pos.y > HEIGHT then asteroid.pos.y = asteroid.pos.y - HEIGHT end
+        if asteroid.pos.y < 0 then asteroid.pos.y = asteroid.pos.y + HEIGHT end
+    end
     
     
-    -- check screen boundaries
-    if pos.x > WIDTH then pos.x = pos.x - WIDTH end
-    if pos.x < 0 then pos.x = pos.x + WIDTH end
-    
-    if pos.y > HEIGHT then pos.y = pos.y - HEIGHT end
-    if pos.y < 0 then pos.y = pos.y + HEIGHT end
     
 end
 
