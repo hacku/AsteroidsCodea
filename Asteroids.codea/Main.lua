b7014277183afa192b1d7f7bc9571a931b044a5d
Philipp Hackbarth <hacku@users.noreply.github.com>
2020-07-22 21:15:30 +0200

    a moving circle

--- Asteroids.codea/Main.lua
+++ Asteroids.codea/Main.lua
@@ -1,19 +1,32 @@
 -- AsteroidsCodea, Philipp Hackbarth 2020
 
--- Use this function to perform your initial setup
+
 function setup()
     print("Hello World!")
+    x = 400
+    y = 50
 end
 
--- This function gets called once every frame
+
 function draw()
-    -- This sets a dark background color 
-    background(40, 40, 50)
 
-    -- This sets the line thickness
-    strokeWidth(5)
-
-    -- Do your drawing here
+    background(2, 2, 2)
+    strokeWidth(1)
+    stroke(255)
+    fill(2)
+    ellipseMode(CENTER)
+    ellipse(x,y,120)             
+    
+    -- move asteroid
+    x = x + 1
+    y = y + 2
+    
+    -- check screen boundaries
+    if x > WIDTH then x = x - WIDTH end
+    if x < 0 then x = x + WIDTH end
+    
+    if y > HEIGHT then y = y - HEIGHT end
+    if y < 0 then y = y + HEIGHT end
     
 end
 
